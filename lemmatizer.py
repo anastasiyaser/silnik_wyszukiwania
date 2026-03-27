f2080c7083ec352a32375e17cca8bffaba33cc7e
anastasiyaser <anastasiya.serebryakova@student.uj.edu.pl>
2026-03-27 13:58:44 +0100

    Update lemmatizer.py

--- lemmatizer.py
+++ lemmatizer.py
@@ -4,9 +4,7 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-# Получаем строку из аргументов
 text = " ".join(sys.argv[1:])
 doc = nlp(text)
 
-# Выводим леммы через пробел
 print(" ".join([token.lemma_ for token in doc]))
